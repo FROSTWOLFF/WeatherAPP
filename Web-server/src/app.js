a3d68f0b8981e003e7c4cf6afcfb1948a001f4eb
Ali Dogus Temizsoy <dogustemizsoy@gmail.com>
2021-05-17 16:29:36 +0300

    HTML and CSS files linked properly

--- Web-server/src/app.js
+++ Web-server/src/app.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const hbs = require('hbs');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
+const publicPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+
+app.set('views', viewsPath);
 app.set('view engine', 'hbs');
 
+app.use('/static', express.static(publicPath));
+
 app.get('/', (req, res) => {
-   res.send('Test world');
+   res.render('index');
+});
+
+app.get('/about', (req, res) => {
+   res.send('About page');
+});
+
+app.get('/help', (req, res) => {
+   res.send('Help page');
 });
 
 app.listen(port, () => {
